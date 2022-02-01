37e0ab33cb60453680fa8c65baa5d29d598aa4b7
Lilibeth Tabares <1762@holbertonschool.com>
2022-02-01 17:54:44 -0500

    Advanced types

---
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -0,0 +1,77 @@
+interface DirectorInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workDirectorTasks(): string;
+}
+
+class Director implements DirectorInterface {
+	workFromHome():string {
+		return ('Working from home');
+	}
+
+	getCoffeeBreak():string {
+		return ('Getting a coffee break');
+	}
+
+	workDirectorTasks():string {
+		return ('Getting to director tasks');
+	}
+}
+
+interface TeacherInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workTeacherTasks(): string;
+}
+
+class Teacher implements TeacherInterface {
+	workFromHome():string {
+		return ('Cannot work from home');
+	}
+	getCoffeeBreak():string {
+		return ('Cannot have a break');
+	}
+	workTeacherTasks():string {
+		return ('Getting to work');
+	}
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+	if (typeof salary === 'number' && salary < 500) {
+		return new Teacher();
+	} else {
+		return new Director();
+	}
+}
+
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
+
+function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+	return employee.workFromHome() === 'Working from home';
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return (employee.workDirectorTasks());
+  } else {
+    return (employee.workTeacherTasks());
+  }
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+	if (todayClass === "Math") {
+		return ('Teaching Math');
+	} else {
+		return ('Teaching History')
+	}
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
