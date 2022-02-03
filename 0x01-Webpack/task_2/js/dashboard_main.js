9681971932a3b6536b9e39c6e400c1ff2c90d3fd
Lilibeth Tabares <1762@holbertonschool.com>
2022-02-03 16:15:42 -0500

    entry

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,26 @@
+import '../css/main.css';
+
+let $ = require("jquery");
+const _ = require("lodash");
+
+let count = 0;
+
+function updateCounter(){
+	count++;
+	$("#count").text(`${count} clicks on the button`);
+}
+
+const $button = $("<button>Click here to get started</button>").on(
+  "click",
+  _.debounce(updateCounter, 500, {
+    leading: true,
+    trailing: false,
+  })
+);
+
+$("body").append("<img id='logo'></img>");
+$("body").append("<p>Holberton Dashboard<p>");
+$("body").append("<p>Dashboard data for the students<p>");
+$("body").append($button);
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School<p>");
