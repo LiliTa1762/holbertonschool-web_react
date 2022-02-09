a635cbae16c82a46cddad976cbe45ac924ad1dc3
Lilibeth Tabares <1762@holbertonschool.com>
2022-02-09 15:16:00 -0500

    updating HMR

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -7,26 +7,35 @@ module.exports = {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, '../dist/')
 	},
+	devServer: {
+		static: './dist',
+	 hot: true,
+	},
 	module: {
-    rules: [
+		rules: [
 			{
-        test: /\.(png|jpg|gif)$/i,
-        use: [
+				test: /\.(tsx|jsx|ts|js)?$/,
+				use: "babel-loader",
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(png|jpg|gif)$/i,
+				use: [
 					'file-loader',
-          {
-            loader: 'image-webpack-loader',
-          },
-        ],
-      },
-      { 
-        test: /\.css$/, 
-        use: ["style-loader", "css-loader"] 
-      },
-    ]
-  },
+					{
+						loader: 'image-webpack-loader',
+					},
+				],
+			},
+			{ 
+				test: /\.css$/, 
+				use: ["style-loader", "css-loader"] 
+			},
+		]
+	},
 	performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000
+		hints: false,
+		maxEntrypointSize: 512000,
+		maxAssetSize: 512000
 },
 };
