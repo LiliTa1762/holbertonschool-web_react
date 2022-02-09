6687f9f26e4e50161dd9d5285326b5c064fa0f40
Lilibeth Tabares <1762@holbertonschool.com>
2022-02-09 17:12:37 -0500

    src code

---
+++ 0x02-react_intro/task_5/dashboard/src/App/App.js
@@ -0,0 +1,27 @@
+import logo from '../assets/holberton_logo.jpeg';
+import './App.css';
+import { getFooterCopy, getFullYear } from '../utils/utils';
+
+function App() {
+  return (
+    <>
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email"></input>
+        <label htmlFor="password">Password</label>
+        <input type="text" name="password" id="password"></input>
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
