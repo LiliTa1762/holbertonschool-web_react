41748dea7acf0ea0de8d486ef26152b8a9deefa5
Lilibeth Tabares <1762@holbertonschool.com>
2022-02-10 15:32:49 -0500

    src code

---
+++ 0x02-react_intro/task_4/dashboard/src/Notifications.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { getLatestNotification } from "./utils";
+import "./Notifications.css";
+import close_icon from "./close_icon.png";
+
+export default function Notifications() {
+	return (
+		<div className="Notifications">
+			<button style={{display: "inline-block",
+        background: "transparent",
+        position: "absolute",
+        border: "none",
+        right: 10,
+				top: 12}}
+				arial-label="Close"
+				onClick={() => console.log('Close button has been clicked')} 
+				>
+				<img src={close_icon} alt="Close icon" style={{width: '23px', height: '23px'}}></img>
+			</button>
+			<p>Here is the list of notifications</p>
+			<ul>
+				<li data="default">New course available</li>
+				<li data="urgent">New resume available</li>
+				<li data="urgent" dangerouslySetInnerHTML={{__html: getLatestNotification()}}></li>
+			</ul>
+		</div>
+	)
+}
