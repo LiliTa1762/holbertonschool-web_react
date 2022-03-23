9caf0f6bd9ada164b93da8041edba95d2c936fa3
Lilibeth Tabares <1762@holbertonschool.com>
2022-03-23 10:49:21 -0500

    UI reducer function in reducers

---
+++ 0x09-react_redux_reducer_selector/task_0/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,19 @@
+import { MARK_AS_READ, SET_TYPE_FILTER } from './notificationActionTypes';
+
+export default function markAsAread(index) {
+	return {
+		type: MARK_AS_READ,
+		index
+	}
+}
+
+export const boundmarkAsAread = (index) => dispatch(markAsAread(index));
+
+export default function setNotificationFilter(filter) {
+	return {
+		type: SET_TYPE_FILTER,
+		filter
+	}
+}
+
+export const boundsetNotificationFilter = (filter) => dispatch(setNotificationFilter(filter));
