89e6e4f959f6d68e38f7451daac299e5204d2ae7
Lilibeth Tabares <1762@holbertonschool.com>
2022-03-29 22:14:19 -0500

    Re-using selectors

---
+++ 0x0A-react_redux_connectors_and_providers/task_6/dashboard/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App/App";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { uiReducer } from './reducers/uiReducer';
+import thunk from 'redux-thunk';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(combineReducers(rootReducer), initialState, composeEnhancers(applyMiddleware(thunk)));
+//Middleware: to add async logic that interacts with the store
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById("root")
+);
